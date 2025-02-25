bca01d8b2b9daf2e7c89e6bfeb847492e42f7d4e
ezrekiel <axel.theule@gmail.com>
2025-02-25 10:20:02 +0100

    changed style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskInput from './TaskInput';
 import TaskList from './TaskList';
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (task) => {
     setTasks([...tasks, { id: Date.now(), text: task, completed: false }]);
@@ -21,13 +28,13 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Gestionnaire de Tâches</h1>
       <TaskInput addTask={addTask} />
-      <TaskList
-        tasks={tasks}
-        toggleTaskCompletion={toggleTaskCompletion}
-        deleteTask={deleteTask}
+      <TaskList 
+        tasks={tasks} 
+        toggleTaskCompletion={toggleTaskCompletion} 
+        deleteTask={deleteTask} 
       />
     </div>
   );
